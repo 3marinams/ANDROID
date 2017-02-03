75778ebb17cf3359ca4e34104784e700b3334721
marinamoreno <lacasitos612>
2017-02-03 15:55:09 +0100

    jjj

--- app/src/main/java/com/idi/marinamoreno/calculadora/SignUpActivity.java
+++ app/src/main/java/com/idi/marinamoreno/calculadora/SignUpActivity.java
@@ -67,46 +67,5 @@ public class SignUpActivity extends Activity  implements View.OnClickListener{
         }
     }
 
-    /*public void OnSignUpClick(View v){
-        if (v.getId() == R.id.sign_up){
-            EditText name = (EditText) findViewById(R.id.name);
-            EditText email = (EditText) findViewById(R.id.email);
-            EditText uname = (EditText) findViewById(R.id.username);
-            EditText pass1 = (EditText) findViewById(R.id.password);
-            EditText pass2 = (EditText) findViewById(R.id.confirm_password);
-
-            String namestr = name.getText().toString();
-            String emailstr = email.getText().toString();
-            String unamestr = uname.getText().toString();
-            String pass1str = pass1.getText().toString();
-            String pass2str = pass2.getText().toString();
-
-            if ( !pass1str.equals(pass2str)){
-                Toast pass = Toast.makeText(SignUpActivity.this, "Les contrasenyes no coincideixen", Toast.LENGTH_SHORT);
-                pass.show();
-            }
-            else {
-                //insert details in DB
-
-                Contact c = new Contact();
-                c.setName(namestr);
-                c.setEmail(emailstr);
-                c.setUsername(unamestr);
-                c.setPassword(pass1str);
-
-                helper = new DataBaseHleper(this);
-                helper.insertContact(c);
-
-                Toast temps = Toast.makeText(SignUpActivity.this, "Ja estàs registrat!", Toast.LENGTH_LONG);
-                temps.show();
-
-                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY| FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i);
-            }
-        }
-
-    }*/
-
 
 }
